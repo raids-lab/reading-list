630f463d65c80a1d60a8819314d9d81da05c1029
Yip Coekjan <cn_yzr@qq.com>
2025-07-03 14:38:02 +0800

    Leverage GitHub Actions to deploy reading list

--- README.md
+++ README.md
@@ -1,50 +1,5 @@
-# Literature on Model Inference Systems
+# Literature for RAIDS Lab
 
-A reading list for model inference systems, including but not limited to related research on software and hardware level. The list covers related papers, articles, tools, blogs, courses and other resources.
+A reading list for RAIDS Lab. The list covers related papers, articles, tools, blogs, courses and other resources.
 
-## Academic Papers
-
-<!--
-****************************************************************************
-*                                                                          *
-*    MAINTANANCE NOTE:                                                     *
-*    - Please ensure that all papers are linked correctly and that the     *
-*      information is up to date.                                          *
-*    - Please order the papers by:                                         *
-*      1. publication date.                                                *
-*      2. conference / journal name.                                       *
-*         - If the paper is not published in a conference or journal,      *
-*           e.g. published on arXiv, please place it behind all other      *
-*           conference / journal papers within the same year.              *
-*      3. alphabetical order of the paper title.                           *
-*                                                                          *
-****************************************************************************
--->
-
-- **[ASPLOS 25]** MoE-Lightning: High-Throughput MoE Inference on Memory-constrained GPUs
-  - Authors: Shiyi Cao, Shu Liu, Tyler Griggs, Peter Schafhalter, Xiaoxuan Liu, Ying Sheng, Joseph E. Gonzalez, Matei Zaharia, and Ion Stoica
-  - [Paper](https://doi.org/10.1145/3669940.3707267), [Code](https://github.com/caoshiyi/artifacts/blob/asplos25)
-- **[ASPLOS 25]** Klotski: Efficient Mixture-of-Expert Inference via Expert-Aware Multi-Batch Pipeline
-  - Authors: Zhiyuan Fang, Yuegui Huang, Zicong Hong, Yufeng Lyu, Wuhui Chen, Yue Yu, Fan Yu, and Zibin Zheng
-  - [Paper](https://doi.org/10.1145/3676641.3716261)
-- **[FAST 25]** Mooncake: Trading More Storage for Less Computation — A KVCache-centric Architecture for Serving LLM Chatbot
-  - Authors: Ruoyu Qin, Zheming Li, Weiran He, Jialei Cui, Feng Ren, Mingxing Zhang, Yongwei Wu, Weimin Zheng, and Xinran Xu
-  - [Paper](https://www.usenix.org/conference/fast25/presentation/qin), [Code](https://github.com/kvcache-ai/Mooncake)
-- **[arXiv 25]** MegaScale-Infer: Serving Mixture-of-Experts at Scale with Disaggregated Expert Parallelism
-  - Authors: Ruidong Zhu, Ziheng Jiang, Chao Jin, Peng Wu, Cesar A. Stuardo, Dongyang Wang, Xinlei Zhang, Huaping Zhou, Haoran Wei, Yang Cheng, Jianzhe Xiao, Xinyi Zhang, Lingjun Liu, Haibin Lin, Li-Wen Chang, Jianxi Ye, Xiao Yu, Xuanzhe Liu, Xin Jin, and Xin Liu
-  - [Paper](https://doi.org/10.48550/arXiv.2504.02263)
-- **[OSDI 24]** DistServe: Disaggregating Prefill and Decoding for Goodput-optimized Large Language Model Serving
-  - Authors: Yinmin Zhong, Shengyu Liu, Junda Chen, Jianbo Hu, Yibo Zhu, Xuanzhe Liu, Xin Jin, and Hao Zhang
-  - [Paper](https://www.usenix.org/conference/osdi24/presentation/zhong-yinmin), [Code](https://github.com/LLMServe/DistServe)
-- **[OSDI 24]** Taming Throughput-Latency Tradeoff in LLM Inference with Sarathi-Serve
-  - Authors: Amey Agrawal, Nitin Kedia, Ashish Panwar, Jayashree Mohan, Nipun Kwatra, Bhargav Gulavani, Alexey Tumanov, and Ramachandran Ramjee
-  - [Paper](https://www.usenix.org/conference/osdi24/presentation/agrawal), [Code](https://github.com/microsoft/sarathi-serve)
-- **[SOSP 24]** PowerInfer: Fast Large Language Model Serving with a Consumer-grade GPU
-  - Authors: Yixin Song, Zeyu Mi, Haotong Xie, and Haibo Chen
-  - [Paper](https://doi.org/10.1145/3694715.3695964), [Code](https://github.com/SJTU-IPADS/PowerInfer)
-- **[SOSP 23]** Efficient Memory Management for Large Language Model Serving with PagedAttention
-  - Authors: Woosuk Kwon, Zhuohan Li, Siyuan Zhuang, Ying Sheng, Lianmin Zheng, Cody Hao Yu, Joseph Gonzalez, Hao Zhang, and Ion Stoica
-  - [Paper](https://doi.org/10.1145/3600006.3613165), [Code](https://github.com/vllm-project/vllm)
-- **[OSDI 22]** Orca: A Distributed Serving System for Transformer-Based Generative Models
-  - Authors: Gyeong-In Yu, Joo Seong Jeong, Geon-Woo Kim, Soojeong Kim, and  Byung-Gon Chun
-  - [Paper](https://www.usenix.org/conference/osdi22/presentation/yu)
+See [the website](https://raids-lab.github.io/reading-list/) for the latest version of the reading list.
---
+++ main.py
@@ -0,0 +1,202 @@
+import argparse
+import bibtexparser
+import pathlib
+import subprocess
+import sys
+from typing import Dict, List, Optional, TextIO
+
+# Define a mapping from shortened book titles to their full names.
+# This is used to identify the series of a literature reference based on its booktitle.
+BOOKTITLE_PATTERN = {
+    "ASPLOS": "Architectural Support for Programming Languages and Operating Systems",
+    "FAST": "File and Storage Technologies",
+    "OSDI": "Operating Systems Design and Implementation",
+    "SOSP": "Symposium on Operating Systems Principles",
+}
+
+
+class LiteratureRef:
+    """
+    A class to represent a literature reference.
+
+    Attributes:
+    - date: publication year
+    - series: conference / journal / book title
+    - title: literature title
+    - authors: list of authors
+    - link: paper url
+    - [optional] code repository url
+    """
+
+    def __init__(
+        self,
+        date: int,
+        series: str,
+        title: str,
+        authors: list[str],
+        link: str,
+        code: Optional[str] = None,
+    ):
+        self.date = date
+        self.series = series
+        self.title = title
+        self.authors = authors
+        self.link = link
+        self.code = code
+
+    def __lt__(self, other: "LiteratureRef") -> bool:
+        """
+        Less than comparison based on date and series.
+        """
+        if self.date != other.date:
+            return self.date > other.date
+        if self.series != other.series:
+            return self.series < other.series
+        return self.title < other.title
+
+    def __repr__(self):
+        return (
+            f"LiteratureRef(date={self.date}, series='{self.series}', "
+            f"title='{self.title}', authors={self.authors}, link='{self.link}', "
+            f"code='{self.code}')"
+        )
+
+    def __str__(self):
+        return (
+            f"[{self.series} {self.date}] {self.title} by {', '.join(self.authors)} " +
+            (f"(Link: {self.link}, Code: {self.code})" if self.code else f"(Link: {self.link})")
+        )
+
+    def into_format(self, fmt: str) -> str:
+        if fmt == "text":
+            return str(self)
+        elif fmt == "html":
+            return f"<strong>[{self.series} {self.date}]</strong> <em>{self.title}</em> by {', '.join(self.authors)}\n" + \
+                f"(<a href='{self.link}'>Link</a>" + \
+                (f", <a href='{self.code}'>Code</a>" if self.code else "") + \
+                ")"
+        else:
+            raise ValueError(f"Unsupported format: {fmt}.")
+
+    @classmethod
+    def parse(cls, bib: TextIO) -> List["LiteratureRef"]:
+        """
+        Parses a BibTeX entry and returns a LiteratureRef object.
+        """
+        bib_data = bibtexparser.load(bib)
+        references = []
+        for entry in bib_data.entries:
+            ref = cls(
+                date=int(entry.get("year", 0)),
+                series=LiteratureRef.__series_of(entry),
+                title=entry.get("title", "").replace("{", "").replace("}", ""),
+                authors=entry.get("author", "").split(" and "),
+                link=entry.get("url", ""),
+                code=entry.get("code", None),
+            )
+            references.append(ref)
+        return references
+
+    @classmethod
+    def __series_of(cls, entry: Dict[str, str]) -> str:
+        if "booktitle" in entry:
+            booktitle = entry["booktitle"]
+            for key, value in BOOKTITLE_PATTERN.items():
+                if value in booktitle:
+                    return key
+            raise ValueError(
+                f"Unknown booktitle: {booktitle}. Please add it to `BOOKTITLE_PATTERN`."
+            )
+        if "journal" in entry:
+            raise NotImplementedError("Journal entries are not supported yet.")
+        if "archiveprefix" in entry:
+            archive_prefix = entry["archiveprefix"]
+            if archive_prefix == "arXiv":
+                return "arXiv"
+            raise ValueError(f"Unknown archivePrefix: {archive_prefix}.")
+        raise ValueError(f"Unknown entry type: {entry}")
+
+
+def output_references(
+    references: List[LiteratureRef],
+    output_stream: TextIO,
+    title: str = "Reading List",
+    fmt: str = "text",
+) -> None:
+    """
+    Outputs the list of references in the specified format.
+    """
+    if fmt == "text":
+        for ref in references:
+            output_stream.write(ref.into_format("text") + "\n")
+    elif fmt == "html":
+        output_stream.write("<!DOCTYPE html>\n")
+        output_stream.write(f"<html><head><title>{title}</title></head>\n")
+        output_stream.write("<body>\n")
+        output_stream.write(f"<h2>{title}</h2>\n")
+        output_stream.write("<ul>\n")
+        # Write each reference as a list item in HTML format
+        # Using the into_format method to convert each reference to HTML
+        for ref in references:
+            output_stream.write(f"<li>{ref.into_format('html')}</li>\n")
+        output_stream.write("</ul>\n</body>\n</html>\n")
+    else:
+        raise ValueError(f"Unsupported format: {fmt}.")
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Reading List Generator via BibTeX parser")
+    parser.add_argument(
+        "-f",
+        "--bib-file",
+        type=str,
+        required=True,
+        metavar="<FILE>",
+        help="Path to the BibTeX file containing literature references",
+    )
+    parser.add_argument(
+        "-F",
+        "--format",
+        type=str,
+        choices=["text", "html"],
+        default="text",
+        help="Output format for the literature references (default: text)",
+    )
+    parser.add_argument(
+        "-O",
+        "--output-file",
+        type=str,
+        metavar="<FILE>",
+        default="-",
+        help="Output file to save the formatted references (default: stdout)",
+    )
+    parser.add_argument(
+        "--open",
+        action="store_true",
+        help="Open the output file after writing (only works if output file is not stdout)",
+    )
+    args = parser.parse_args()
+
+    if args.open and args.output_file == "-":
+        parser.error("Cannot open stdout. Please specify an output file.")
+
+    with open(args.bib_file, "r", encoding="utf-8") as bib_file:
+        references = LiteratureRef.parse(bib_file)
+    references.sort()
+
+    # Convert kebab case to title case for the output title
+    title = pathlib.Path(args.bib_file).stem.replace("-", " ").title()
+
+    if args.output_file == "-":
+        output_references(references, sys.stdout, title=title, fmt=args.format)
+    else:
+        with open(args.output_file, "w", encoding="utf-8") as output_stream:
+            output_references(references, output_stream,
+                              title=title, fmt=args.format)
+            if args.open:
+                subprocess.run(["xdg-open", args.output_file])
+
+
+if __name__ == "__main__":
+    main()
